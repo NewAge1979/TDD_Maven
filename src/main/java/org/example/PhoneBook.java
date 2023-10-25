08987cc477328995ba2811fae56767c9e71f3044
Max Khrapatiy <max.khrapatiy@gmail.com>
2023-10-25 23:43:11 +0300

    findByNumber implemented.

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -2,9 +2,11 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PhoneBook {
     private final Map<String, String> myPhoneBook = new HashMap<>();
+
     public int add(String name, String phone) {
         if (name != null && !name.isEmpty() && phone != null && !phone.isEmpty()) {
             myPhoneBook.put(name, phone);
@@ -13,6 +15,17 @@ public class PhoneBook {
     }
 
     public String findByNumber(String phone) {
-        return null;
+        String result = "Укажите номер телефона.";
+        if (phone != null && !phone.isEmpty()) {
+            result = myPhoneBook
+                    .entrySet()
+                    .stream()
+                    .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey))
+                    .get(phone);
+            if (result == null) {
+                result = "Абонент не найден.";
+            }
+        }
+        return result;
     }
 }
