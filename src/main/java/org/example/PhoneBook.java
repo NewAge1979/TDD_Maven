469b1e02ed600dc5d0f574717c188c9fedf0abd1
Max Khrapatiy <max.khrapatiy@gmail.com>
2023-10-25 23:04:26 +0300

    add implemented.

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> myPhoneBook = new HashMap<>();
     public int add(String name, String phone) {
-        return 0;
+        if (name != null && !name.isEmpty() && phone != null && !phone.isEmpty()) {
+            myPhoneBook.put(name, phone);
+        }
+        return myPhoneBook.size();
     }
 }
